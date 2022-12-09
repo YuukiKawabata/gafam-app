1c90d7440a7d2e86409c310f8967291212f9af3d
{Yuuki} <{kakabata.man@gmail.com}>
2022-12-09 18:34:43 +0900

    データファイルから出力

--- pages/answer.js
+++ pages/answer.js
@@ -1,13 +1,20 @@
 import Link from "next/link";
+import { data } from "./data";
 
 export default function answer() {
+  // let i = Math.floor(Math.random() * 3);
+
+  const lists = data.Lists;
+  console.log(lists[0].name);
+  console.log(lists[0].value);
+
   return (
     <div className="h-screen w-screen justify-center items-center">
       <div className="h-1/6 text-4xl text-center tracking-wider flex justify-center items-center sm:text-6xl">
-        <h1>名前</h1>
+        <h1>{lists[1].name}</h1>
       </div>
       <div className="h-4/6 text-center leading-10 text-lg tracking-widest flex flex-wrap justify-center items-center sm:text-2xl">
-        <p className="">test test test</p>
+        <p className="">{lists[1].value}</p>
       </div>
 
       <Link href="/">
@@ -17,4 +24,8 @@ export default function answer() {
       </Link>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  return { props: { data } };
 }
