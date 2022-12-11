09eafd92609666b6e1f58318ea1200145667da91
{Yuuki} <{kakabata.man@gmail.com}>
2022-12-11 14:48:40 +0900

    戻し

---
+++ components/Title.js
@@ -0,0 +1,8 @@
+export function Title(props) {
+    return (
+        <div className="h-1/6 text-4xl text-center tracking-wider flex justify-center items-center sm:text-6xl">
+          <h1 className="">{props.text}
+          </h1>
+        </div>
+    );
+  }
