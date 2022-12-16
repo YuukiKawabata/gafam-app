22e05a50f32a21b7602c4b09ac8e121bbec27d5c
{Yuuki} <{kakabata.man@gmail.com}>
2022-12-16 09:20:38 +0900

    ランダム数字増加、文字数が多い時のレイアウト修正

--- pages/answer.js
+++ pages/answer.js
@@ -83,12 +83,87 @@ export default function Answer() {
       value:
         "夢を持つなら、とびきり野心的な方がいい。その方が前に進めるから",
     },
+    {
+      name: "グーグル共同創業者セルゲイ・ミハイロヴィッチ・ブリン",
+      value:
+        "あまりにも多くのルールを採用すると革新は阻害されてしまう。",
+    },
+    {
+      name: "グーグル共同創業者セルゲイ・ミハイロヴィッチ・ブリン",
+      value:
+        "アイデアを思いつくことは何か素晴らしいものを作ることの中で最も大事な部分ではありません。一番大事なのは実行することです。",
+    },
+    {
+      name: "グーグル共同創業者セルゲイ・ミハイロヴィッチ・ブリン",
+      value:
+        "厳しい時代こそ最高のものが生み出される。",
+    },
+    {
+      name: "グーグル共同創業者セルゲイ・ミハイロヴィッチ・ブリン",
+      value:
+        "人々は常に新しいものを求めます。",
+    },
+    {
+      name: "グーグル共同創業者セルゲイ・ミハイロヴィッチ・ブリン",
+      value:
+        "今の時代に成功する人は非常に洗練されていなければなりません。",
+    },
+    {
+      name: "グーグル共同創業者セルゲイ・ミハイロヴィッチ・ブリン",
+      value:
+        "成功はシンプルから生まれる。",
+    },
+    {
+      name: "マーク・ザッカーバーグ",
+      value:
+        "同じTシャツしか着ない",
+    },
+    {
+      name: "マーク・ザッカーバーグ",
+      value:
+        "世界の富豪でありながら、給料は年間1ドル",
+    },
+    {
+      name: "マーク・ザッカーバーグ",
+      value:
+        "私たちの世代の挑戦は新しい仕事を作り出すことだけでなく、新しい目的意識を作り出すことだ",
+    },
+    {
+      name: "マーク・ザッカーバーグ",
+      value:
+        "偉大な成功は 、失敗する自由から生まれる",
+    },
+    {
+      name: "マーク・ザッカーバーグ",
+      value:
+        "変化というのは、身近な場所から始まる。世界的な変化だって、私たちのような小さな存在から始まる",
+    },
+    {
+      name: "マーク・ザッカーバーグ",
+      value:
+        "完璧であることより、まず終わらせることが重要だ",
+    },
+    {
+      name: "マーク・ザッカーバーグ",
+      value:
+        "最大のリスクは、リスクを取らないことだ",
+    },
+    {
+      name: "マーク・ザッカーバーグ",
+      value:
+        "一番大事なのは、まずはやってみようということ。",
+    },
+    {
+      name: "マーク・ザッカーバーグ",
+      value:
+        "「お金がないならアイデアを出せ」",
+    },
 
 
   ];
 
   //ランダムな数字を出して、数字を元に名言を出す
-  const [number, setNumber] = useState(Math.floor(Math.random() * 16));
+  const [number, setNumber] = useState(Math.floor(Math.random() * 31));
 console.log(lists)
   const name = lists[number].name;
   const content = lists[number].value;
@@ -96,10 +171,10 @@ console.log(lists)
 
   return (
     <div className="h-screen w-screen justify-center items-center">
-      <div className="h-1/6 p-6 text-4xl text-center tracking-wider flex justify-center items-center sm:text-8xl">
+      <div className="h-2/6 p-6 text-4xl text-center tracking-wider flex justify-center items-center sm:text-8xl">
         <Title text={name} />
       </div>
-      <div className="h-4/6 p-6 text-center leading-10 text-base tracking-widest flex flex-wrap justify-center items-center sm:text-4xl">
+      <div className="h-3/6 p-6 text-center leading-10 text-base tracking-widest flex flex-wrap justify-center items-center sm:text-4xl">
         <p>『{content}』</p>
       </div>
 
