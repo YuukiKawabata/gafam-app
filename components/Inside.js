7c484a1e43ff0a4f50e6cfd656f071bef15395d9
{Yuuki} <{kakabata.man@gmail.com}>
2022-12-28 12:07:27 +0900

    favicon設定

--- components/Inside.js
+++ components/Inside.js
@@ -1,6 +1,6 @@
 export function Inside() {
   return (
-    <div className="h-3/6 text-center text-lg tracking-widest flex flex-wrap justify-center items-center sm:text-4xl">
+    <div className="h-3/6 p-3 text-center text-lg tracking-widest flex flex-wrap justify-center items-center sm:text-4xl">
       <p className="p-2">興奮する名言を心に刻む。</p>
       <p className="p-2">そして,自分を奮い立たせよ。</p>
       <p className="p-2">何か行動したい時、</p>
