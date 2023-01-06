12adeb1b129e62c6c07e8fc9607c9ee0e78133a7
{Yuuki} <{kakabata.man@gmail.com}>
2023-01-06 15:04:38 +0900

    favicon設定

--- pages/index.js
+++ pages/index.js
@@ -10,27 +10,6 @@ export default function Home() {
       <Head>
         <title>ビッグ・テック名言</title>
         <meta name="description" content="心に響く名言で前に進もう" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c"></meta>
-        <meta name="theme-color" content="#fcf9f9"></meta>
       </Head>
 
       <Title text="ビッグ・テックを愛する人へ" />
