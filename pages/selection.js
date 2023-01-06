a9464a551c9acc28ed28bde89f243b5c5d7bb1cb
{Yuuki} <{kakabata.man@gmail.com}>
2023-01-06 20:41:14 +0900

    スクロールさせない

--- pages/selection.js
+++ pages/selection.js
@@ -1,12 +1,21 @@
 import Link from "next/link";
+import { useEffect } from "react";
 import { Button } from "../components/Button";
 import { Title } from "../components/Title";
 
 export default function selection() {
+  const selectionLists = [
+    { button: "頑張りたい" },
+    { button: "励まして" },
+    { button: "行くぞ！！" },
+    { button: "疲れたよ。。" },
+    { button: "少し休憩" },
+    { button: "なんでも" },
+  ];
+
   return (
     <div className="h-screen w-screen justify-center items-center">
       <Title text="今の気持ちは？" />
-
 
       <div className="h-3/6 text-center text-lg tracking-widest flex flex-wrap justify-center items-center sm:text-2xl">
         <Link href="/makeEffort">
