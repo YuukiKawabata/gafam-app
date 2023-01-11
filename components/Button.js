693fa27e732628a3af44725f43e21007a2e18e9f
{Yuuki} <{kakabata.man@gmail.com}>
2023-01-11 17:22:59 +0900

    consol.log削除

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,6 @@ import React from "react";
 
 
 export function Button(props) {
-  console.log(props.href);
   return (
     <div className="h-1/6 justify-center items-center text-base sm:text-4xl">
       <Link href={`${props.href}`}>
