2ac2cb28025f5953c16cbe9e7a76cea27e587a52
{Yuuki} <{kakabata.man@gmail.com}>
2023-01-26 22:49:44 +0900

    ボタンを変更

--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import React from "react";
 
-
 export function Button(props) {
   return (
     <div className="mt-5 whitespace-pre-wrap leading-loose font-black h-1/6 p-5 text-base text-center tracking-wider  justify-center items-center  sm:text-2xl ">
-
       <Link href={`${props.href}`}>
-        <button className="button block ">{props.text}</button>
+        <button className="button block">
+          <span>{props.text} </span>
+        </button>
       </Link>
     </div>
   );
 }
+
